dc9abd61da0436f62f744eb966f77ea647242673
MahendraDevid <mahendradevidrpl@gmail.com>
2023-01-27 13:40:21 +0700

    menghapus 5 file yang ga penting

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from models import siswa
-
-class SiswaAdmin(admin.ModelAdmin):
-    list_display = ['nama', 'email', 'hp' ,'alamat', 'jenis_kelamin', 'tempat_lahir', 'tanggal_lahir', 'nisn']
-
-admin.site.register(siswa, SiswaAdmin)
